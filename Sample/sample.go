e3d38ca11fcb9bff9150bdd03e0d77573b5306e2
Okerachuchupurin <145318409+MashuNakamura@users.noreply.github.com>
2025-04-28 08:06:48 +0700

    Progress

--- Sample/sample.go
+++ Sample/sample.go
@@ -6,13 +6,34 @@ import (
 )
 
 func main() {
+	// Example of a string variable
 	var complex = 1 + 1
 	var tipe = reflect.TypeOf(complex)
+	var acumalaka string
+	test := "Hello World!"
 
+	// Example of a number variable
 	fmt.Println("This is a sample Go program.")
 	fmt.Println("Satu = ", 1)
 	fmt.Println("Dua = ", 2)
 	fmt.Println("Tiga = ", 3)
+
+	fmt.Println()
+	// Example calling a string variable
 	fmt.Println(complex)
 	fmt.Println(tipe)
+	fmt.Println("Penamaan Var", acumalaka)
+	fmt.Println(test)
+
+	fmt.Println()
+
+	// Example of a boolean variable
+	fmt.Println("True", true)
+	fmt.Println("False", false)
+
+	// Get the length of a string
+	fmt.Println(len("Hello World!"))
+
+	// Get the index of the first character in a string
+	fmt.Println("Hello World"[0])
 }
